f7cadb53407e9bcb6eb44b6a9769e305cb7e3d60
Clément Laigle <c.laigle@catie.fr>
2023-11-08 16:57:58 +0100

    Add 6TRON bus definition

---
+++ boards/arm/zest_core_stm32l4a6rg/6tron-header.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2023 CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_6TRON_HEADER_H_
+#define INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_6TRON_HEADER_H_
+
+/**
+ * @name 6TRON pin identifiers
+ * @{
+ */
+#define P1_CAN_RX 0
+#define P1_CAN_TX 1
+#define P1_I2C_SDA 2
+#define P1_I2C_SCL 3
+#define P1_UART_RX 4
+#define P1_UART_TX 5
+#define P1_SPI_MOSI 6
+#define P1_SPI_MISO 7
+#define P1_SPI_SCK 8
+#define P1_SPI_SS 9
+#define P1_PWM1 10
+#define P1_PWM2 11
+#define P1_PWM3 12
+#define P1_PWM4 13
+#define P1_ICAP 14
+#define P1_WKUP 15
+#define P1_ADC1 16
+#define P1_ADC2 17
+#define P1_ADC3 18
+#define P1_ADC4 19
+#define P1_DAC 20
+#define P1_DIO1 21
+#define P1_DIO2 22
+#define P1_DIO3 23
+#define P1_DIO4 24
+#define P1_DIO5 25
+#define P1_DIO6 26
+#define P1_DIO7 27
+#define P1_DIO8 28
+#define P1_DIO9 29
+#define P1_DIO10 30
+#define P1_DIO11 31
+#define P1_DIO12 32
+#define P1_DIO13 33
+#define P1_DIO14 34
+#define P1_DIO15 35
+#define P1_DIO16 36
+#define P1_DIO17 37
+#define P1_DIO18 38
+#define P1_DIO19 39
+#define P1_DIO20 40
+
+/** @} */
+
+#endif /* INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_6TRON_HEADER_H_ */
